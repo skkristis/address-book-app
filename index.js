bfc173461db4328fec83e942ccfed0e0a5783112
Kristoferis Solovjov <skkristis@gmail.com>
2022-09-05 21:48:59 +0300

    basic interface

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("hello world!");
+const addressBook = [];
+
+document.querySelector("#address-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+  console.log(Object.fromEntries(formData));
+});
