3bebacd9eaecc8384f00aea5e15d4dd026c29f07
Kristoferis Solovjov <skkristis@gmail.com>
2022-09-07 14:16:17 +0300

    customId authToken

--- index.js
+++ index.js
@@ -1,18 +1,106 @@
 let addressBook = [];
 
-const render = (addressArray) => {
-  function createBlock(info, type = "div", name, number) {
+document.querySelector("#address-form").addEventListener("click", (e) => {
+  if (e.target.id === "clear") {
+    document.querySelector("#address-form").reset();
+  }
+
+  document.querySelector("#address-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const fakeObj = {
+      name: null,
+      phoneNumber: null,
+      address: null,
+      email: null,
+      fav: false,
+    };
+
+    const formData = new FormData(e.target);
+    let address = Object.fromEntries(formData);
+    if (address.name && address["phoneNumber"]) {
+      address = Object.assign({}, fakeObj, address);
+      addressBook.push(address);
+      document.querySelector("#address-form").reset();
+
+      localStorage.setItem("addressBook", JSON.stringify(addressBook));
+      render(addressBook);
+    }
+  });
+});
+
+document.querySelector("#search-bar").addEventListener("keyup", (e) => {
+  const renderArr = addressBook.filter((x) => {
+    for (let key in x) {
+      if (key !== "fav") {
+        if (x[key].includes(e.target.value)) {
+          return true;
+        }
+      } else if (key == "fav" && e.target.value == "favorites") {
+        return x[key];
+      }
+    }
+    return false;
+  });
+  render(renderArr);
+});
+
+document.querySelector("#output").addEventListener("click", (e) => {
+  function selectedHandlers(remove, edit) {
+    if (document.querySelector(".selectedHandlers")) {
+      document.querySelector(".selectedHandlers").remove();
+    }
+    if (remove) {
+      const item = document.createElement("button");
+      item.type = "button";
+      item.innerText = "Delete selected";
+      item.className = "selectedHandlers";
+      document.querySelector("#output").prepend(item);
+    } else if (edit) {
+      const item = document.createElement("button");
+      item.type = "button";
+      item.innerText = "Save edit";
+      item.className = "selectedHandlers";
+      document.querySelector("#output").prepend(item);
+    }
+  }
+
+  if (e.target.id === "selected-handler") {
+    if (e.target.checked) {
+      document.querySelectorAll(".selected").forEach((x) => (x.checked = true));
+      selectedHandlers(false, true);
+    } else {
+      document.querySelectorAll(".selected").forEach((x) => (x.checked = false));
+      document.querySelector(".selectedHandlers").remove();
+    }
+  } else if (e.target.className === "delete") {
+    const index = addressBook.findIndex((person) => person.customId === e.target.name);
+    addressBook.splice(index, 1);
+    e.target.parentNode.parentNode.remove();
+    localStorage.setItem("addressBook", JSON.stringify(addressBook));
+  } else if (e.target.className === "edit") {
+    render(addressBook, true);
+    selectedHandlers(false, true);
+    document.querySelector("#output").prepend();
+  } else if (e.target.tagName === "BUTTON" && e.target.name) {
+    const index = addressBook.findIndex((person) => person.customId === e.target.name);
+    addressBook[index].fav === "on" ? (addressBook[index].fav = false) : (addressBook[index].fav = "on");
+    e.target.innerText === "" ? (e.target.innerText = "☆") : (e.target.innerText = "");
+    localStorage.setItem("addressBook", JSON.stringify(addressBook));
+  }
+});
+
+const render = (addressArray, edit) => {
+  function createBlock(info, type = "div", customId) {
     const container = document.createElement(type);
-    const customId = name + number;
-    container.innerText = info;
+    type === "input" ? (container.value = info) && (container.type = "text") : (container.innerText = info);
     container.name = customId;
     return container;
   }
-  function handlers(obj, name, number) {
+  function handlers(obj, customId) {
     const container = document.createElement("div");
     const remove = document.createElement("button");
     const edit = document.createElement("button");
-    const customId = name + number;
     remove.type = "button";
     edit.type = "button";
     remove.className = "delete";
@@ -27,6 +115,7 @@ const render = (addressArray) => {
 
     return container;
   }
+
   if (document.querySelectorAll(".address")) {
     document.querySelector("#output-container").innerHTML = null;
   }
@@ -36,6 +125,7 @@ const render = (addressArray) => {
   addressArray.forEach((obj) => {
     const container = document.createElement("div");
     const checkbox = document.createElement("input");
+    const customId = obj.name + obj.phoneNumber;
 
     container.className = "address";
     checkbox.type = "checkbox";
@@ -46,79 +136,15 @@ const render = (addressArray) => {
     for (let prop in obj) {
       const info = obj[prop];
       if (prop === "fav") {
-        container.appendChild(createBlock(obj[prop] ? "☆" : "", "button", obj.name, obj.phoneNumber));
+        container.appendChild(createBlock(obj[prop] ? "☆" : "", "button", customId));
       } else if (prop !== "customId") {
-        container.appendChild(createBlock(info));
+        container.appendChild(createBlock(info, edit ? "input" : null));
       }
       output.appendChild(container);
     }
-    container.appendChild(handlers(obj, obj.name, obj.phoneNumber));
+    container.appendChild(handlers(obj, customId));
   });
 };
-
-document.querySelector("#address-form").addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const fakeObj = {
-    name: null,
-    phoneNumber: null,
-    address: null,
-    email: null,
-    fav: false,
-  };
-
-  const formData = new FormData(e.target);
-  let address = Object.fromEntries(formData);
-  if (address.name && address["phoneNumber"]) {
-    address = Object.assign({}, fakeObj, address);
-    addressBook.push(address);
-    document.querySelector("#address-form").reset();
-
-    localStorage.setItem("addressBook", JSON.stringify(addressBook));
-    render(addressBook);
-  }
-});
-
-document.querySelector("#clear").addEventListener("click", (e) => {
-  document.querySelector("#address-form").reset();
-});
-
-document.querySelector("#output").addEventListener("click", (e) => {
-  if (e.target.id === "selected-handler") {
-    if (e.target.checked) {
-      document.querySelectorAll(".selected").forEach((x) => (x.checked = true));
-    } else {
-      document.querySelectorAll(".selected").forEach((x) => (x.checked = false));
-    }
-  } else if (e.target.className === "delete") {
-    const index = addressBook.findIndex((person) => person.customId === e.target.name);
-    addressBook.splice(index, 1);
-    e.target.parentNode.parentNode.remove();
-    localStorage.setItem("addressBook", JSON.stringify(addressBook));
-  } else if (e.target.className === "edit") {
-    console.log(e.target.name);
-  } else if (e.target.tagName === "BUTTON") {
-    const index = addressBook.findIndex((person) => person.customId === e.target.name);
-    addressBook[index].fav === "on" ? (addressBook[index].fav = false) : (addressBook[index].fav = "on");
-    e.target.innerText === "" ? (e.target.innerText = "☆") : (e.target.innerText = "");
-    localStorage.setItem("addressBook", JSON.stringify(addressBook));
-  }
-});
-
-document.querySelector("#search-bar").addEventListener("keyup", (e) => {
-  const renderArr = addressBook.filter((x) => {
-    for (let key in x) {
-      if (key !== "fav") {
-        if (x[key].includes(e.target.value)) {
-          return true;
-        }
-      } else if (key == "fav" && e.target.value == "favorites") {
-        return x[key];
-      }
-    }
-  });
-  render(renderArr);
-});
 
 (function () {
   if (localStorage.addressBook) {
